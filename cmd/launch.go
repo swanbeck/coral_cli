a6cd1b5d292026d4dc8a3182f28c8f21e4caa671
Steven Swanbeck <steven.swanbeck@gmail.com>
2025-05-13 13:49:37 -0500

    properly handling .env and compose.yaml files and absences

--- cmd/launch.go
+++ cmd/launch.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 	// "path/filepath"
 
 	"github.com/spf13/cobra"
@@ -28,18 +29,27 @@ var launchCmd = &cobra.Command{
 }
 
 func init() {
-	launchCmd.Flags().StringVarP(&composePath, "compose-file", "f", "", "Path to compose.yaml")
+	launchCmd.Flags().StringVarP(&composePath, "compose-file", "f", "", "Path to Docker Compose .yaml file")
 	launchCmd.Flags().StringVarP(&envFile, "env-file", "e", "", "Path to .env file")
-	launchCmd.MarkFlagRequired("compose-file")
 }
 
 func launch(composePath, envFile string) error {
-	env, err := compose.LoadEnv(envFile)
-	if err != nil {
-		return fmt.Errorf("loading .env: %w", err)
+	env := make(map[string]string)
+	resolvedEnvFile, err := resolveEnvFile(envFile)
+	if resolvedEnvFile != "" {
+		var err error
+		env, err = compose.LoadEnv(resolvedEnvFile)
+		if err != nil {
+			return fmt.Errorf("loading .env: %w", err)
+		}
 	}
 
-	cf, err := compose.ParseCompose(composePath, env)
+	pathToUse, err := resolveComposeFile(composePath)
+	if err != nil {
+		return err
+	}
+
+	cf, err := compose.ParseCompose(pathToUse, env)
 	if err != nil {
 		return fmt.Errorf("parsing compose file: %w", err)
 	}
@@ -50,13 +60,36 @@ func launch(composePath, envFile string) error {
 	}
 
 	for name, service := range cf.Services {
-		fmt.Printf("Extracting image for service: %s\n", name)
-		fmt.Printf("Corresponding image: %s\n", service.Image)
-		// err := extractor.ExtractImage(service.Image, "darwin-"+name, configPath)
+		fmt.Printf("Extracting image for service: %s with corresponding image: %s\n", name, service.Image)
+		err := extractor.ExtractImage(service.Image, "darwin-"+name, configPath)
 		// if err != nil {
 		// 	return fmt.Errorf("failed to extract image %s: %w", service.Image, err)
 		// }
 	}
 
 	return nil
+}
+
+func resolveComposeFile(userPath string) (string, error) {
+	if userPath != "" {
+		return userPath, nil
+	}
+	candidates := []string{"docker-compose.yaml", "compose.yaml", "docker-compose.yml", "compose.yml"}
+	for _, f := range candidates {
+		if _, err := os.Stat(f); err == nil {
+			return f, nil
+		}
+	}
+	return "", fmt.Errorf("no compose file found (tried: docker-compose.yaml, compose.yaml, docker-compose.yml, compose.yml)")
+}
+
+func resolveEnvFile(userPath string) (string, error) {
+	if userPath != "" {
+		return userPath, nil
+	}
+	if _, err := os.Stat(".env"); err == nil {
+		return ".env", nil
+	}
+	// just returning empty string if no .env file
+	return "", nil
 }
